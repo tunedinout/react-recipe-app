f4eef26976ff148c8c0165e65dd4266c2d87ca48
tunedinout <narottam2911@gmail.com>
2020-02-07 16:25:23 +0530

    add all buttons on header bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,46 @@ import './header.css'
 
 
 class Header extends React.Component{
+    constructor(){
+        super();
+        this.onAuthClick = this.onAuthClick.bind(this);
+        this.onRecpClick = this.onRecpClick.bind(this);
+        this.onShopClick = this.onShopClick.bind(this);
+        this.onLogoutClick = this.onLogoutClick.bind(this);
+        this.state = {
+            authBgColor : "none",
+            recpBgColor : "none",
+            shopBgColor : "none"
+
+        }
+    }
+    onAuthClick(event){
+        
+        this.setState({ authBgColor: "rgb(212, 217, 213)",
+                        shopBgColor: "none",
+                        recpBgColor: "none"
+                    })
+        console.log(event);
+    }
+    onRecpClick(event){
+
+        this.setState({
+            recpBgColor: "rgb(212, 217, 213)",
+            shopBgColor: "none",
+            authBgColor: "none"})
+        console.log(event);
+        
+    }
+    onShopClick(event){
+        this.setState({
+            shopBgColor: "rgb(212, 217, 213)",
+            recpBgColor: "none",
+            authBgColor: "none"})
+        console.log(event);
+    }
+    onLogoutClick(event){
+
+    }
     render(){
         return (
             
@@ -15,21 +55,29 @@ class Header extends React.Component{
                
             </div>
 
-            <div className="recipes">
+            <div className="auth" onClick={this.onAuthClick} style={{backgroundColor:this.state.authBgColor}}>
+                    <div className="auth-txt"> 
+                        Authentication
+                    </div>
+               
+            </div>
+
+            {/* hide if not authenticated */}
+            <div className="recipes" onClick={this.onRecpClick} style={{backgroundColor:this.state.recpBgColor}}>
                 <div className="recipes-txt">
                     Recipes
                 </div>
                 
             </div>
 
-            <div className="shopping-list">
+            <div className="shopping-list" onClick={this.onShopClick} style={{backgroundColor:this.state.shopBgColor}}>
                 <div className="shopping-list-txt">
                     Shopping List    
                 </div>
                    
               
             </div>
-            <div className="logout">
+            <div className="logout" onClick={this.onLogoutClick}>
                 <div className="logout-txt">
                     Logout
                 </div>
